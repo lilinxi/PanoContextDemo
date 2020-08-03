6a7095f89ac77892f946a8c9e0ef5846ae766556
limengfan.wlj <limengfan.wlj@bytedance.com>
2020-08-03 21:37:29 +0800

    test ignore

--- demos/demo_pano_line_draw.py
+++ demos/demo_pano_line_draw.py
@@ -4,13 +4,13 @@ import numpy as np
 import Projection
 import Visualization
 
-panoImage = cv2.imread("../images/360_1.jpg", cv2.IMREAD_COLOR)
+panoImage = cv2.imread("../images/360.jpg", cv2.IMREAD_COLOR)
 edges = np.full(panoImage.shape, 255, dtype=np.uint8)
 
 projectImageAndMapping = Projection.CubeProjection(panoImage, 500)
 
 for i in range(len(projectImageAndMapping)):
-    cv2.imwrite("output_cube_project_" + str(i) + ".jpg", projectImageAndMapping[i][0])
+    cv2.imwrite("output_cube_project_2_" + str(i) + ".jpg", projectImageAndMapping[i][0])
     projectImage=projectImageAndMapping[i][0]
     mapping=projectImageAndMapping[i][1]
     projectImageGray = cv2.cvtColor(projectImage, cv2.COLOR_BGR2GRAY)
@@ -19,5 +19,5 @@ for i in range(len(projectImageAndMapping)):
     Visualization.DrawPanoLine(panoImage, lines, mapping, (255, 0, 0))
     Visualization.DrawPanoLine(edges, lines, mapping, (255, 0, 0))
 
-cv2.imwrite("output_pano_lsd.jpg", panoImage)
-cv2.imwrite("output_pano_edges.jpg", edges)
+cv2.imwrite("output_pano_lsd_2.jpg", panoImage)
+cv2.imwrite("output_pano_edges_2.jpg", edges)
