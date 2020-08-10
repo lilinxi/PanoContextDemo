896dc102acc1230f7eb70c5725ea44f6ee9b63a6
limengfan.wlj <limengfan.wlj@bytedance.com>
2020-08-10 18:59:58 +0800

    daily add at 2020-08-10 18:59:58

--- demos/demo_pano_hough_great_circle_vp_segmentation.py
+++ demos/demo_pano_hough_great_circle_vp_segmentation.py
@@ -1,12 +1,12 @@
 import cv2
 import VpEstimation
 
-# panoImage = cv2.imread("../images/360_1.jpg", cv2.IMREAD_COLOR)
-# panoImage = cv2.imread("../dataset-good/dataset-" + str(13) + ".jpg", cv2.IMREAD_COLOR)
-# print(panoImage.shape)
-# VpEstimation.HoughGreatCircleVpEstimationSegmentation(panoImage, "13_test_2_3")
+panoImage = cv2.imread("../images/360_1.jpg", cv2.IMREAD_COLOR)
+panoImage = cv2.imread("../dataset-good/dataset-" + str(0) + ".jpg", cv2.IMREAD_COLOR)
+print(panoImage.shape)
+VpEstimation.HoughGreatCircleVpEstimationSegmentation(panoImage, "canny_1")
 
-for i in range(1, 30):
-    panoImage = cv2.imread("../dataset-good/dataset-" + str(i) + ".jpg", cv2.IMREAD_COLOR)
-    print(panoImage.shape)
-    VpEstimation.HoughGreatCircleVpEstimationSegmentation(panoImage, logid="tt5" + str(i))
+# for i in range(1, 30):
+#     panoImage = cv2.imread("../dataset-good/dataset-" + str(i) + ".jpg", cv2.IMREAD_COLOR)
+#     print(panoImage.shape)
+#     VpEstimation.HoughGreatCircleVpEstimationSegmentation(panoImage, logid="tt5" + str(i))
--- demos/demo_pano_line_index.py
+++ demos/demo_pano_line_index.py
@@ -1,12 +1,12 @@
 import cv2
 import VpEstimation
 
-# panoImage = cv2.imread("../images/360_1.jpg", cv2.IMREAD_COLOR)
-# panoImage = cv2.imread("../dataset-good/dataset-" + str(13) + ".jpg", cv2.IMREAD_COLOR)
-# print(panoImage.shape)
-# VpEstimation.HoughGreatCircleVpEstimationSegmentationAndLineIndex(panoImage, "line_index_360_1")
+panoImage = cv2.imread("../images/360_1.jpg", cv2.IMREAD_COLOR)
+panoImage = cv2.imread("../dataset-good/dataset-" + str(13) + ".jpg", cv2.IMREAD_COLOR)
+print(panoImage.shape)
+VpEstimation.HoughGreatCircleVpEstimationSegmentationAndLineIndex(panoImage, "line_index_360_1")
 
-for i in range(1, 30):
-    panoImage = cv2.imread("../dataset-good/dataset-" + str(i) + ".jpg", cv2.IMREAD_COLOR)
-    print(panoImage.shape)
-    VpEstimation.HoughGreatCircleVpEstimationSegmentationAndLineIndex(panoImage, logid="line_index_" + str(i))
+# for i in range(1, 30):
+#     panoImage = cv2.imread("../dataset-good/dataset-" + str(i) + ".jpg", cv2.IMREAD_COLOR)
+#     print(panoImage.shape)
+#     VpEstimation.HoughGreatCircleVpEstimationSegmentationAndLineIndex(panoImage, logid="line_index_" + str(i))
